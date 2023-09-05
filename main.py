ecd53dabf74782646150f3fa83c4aaf299373b4b
polten0 <alexey.mikryukov@mail.ru>
2023-09-05 09:53:46 +0300

    initCommit

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-print('Здравствуй, мир!')
-print()
-
-print(4, 8, 15, 16, 23, 42)
-print()
-
-print(4)
-print(8)
-print(15)
-print(16)
-print(23)
-print(42)
-print()
-
-for i in range(7):
-    print('*' * (i+1))
+print(input(), '-', 'чемпион!')
